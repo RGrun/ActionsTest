92bcb45c94e61186a5b02629bd2e37ae4fb39148
R G <richard@furu.guru>
2022-08-11 13:29:17 -0700

    initial

---
+++ ActionsTest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Newtonsoft.Json;
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+}
+
+TestObj testObj = new TestObj()
+{
+    TestOne = "1",
+    TestTwo = "two",
+    TestThree = 3,
+};
+
+Console.WriteLine(JsonConvert.SerializeObject(testObj));
+
+Console.ReadLine();
+
+class TestObj
+{
+    public string TestOne;
+    public string TestTwo;
+    public int TestThree;
+}
